9509e60c6389c909b8924c5fb6775385c6ffb0e0
ozer0532 <ozer0532@planet.earth>
2020-06-09 17:01:39 +0700

    Load data on client load start

--- suspect-expander/src/App.js
+++ suspect-expander/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
 
 import Box from '@material-ui/core/Box'
 import Container from '@material-ui/core/Container'
 
-import GetData from './getData'
+import { GetData, GetAllPerson } from './getData'
 import SearchInput from './searchInput'
 
 const styles = {
@@ -21,12 +19,37 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      personList: [],
+    };
+    console.log(this.state);
+    this.GetPersonList = this.GetPersonList.bind(this);
+    this.onSearch = this.onSearch.bind(this);
 
-    }
+    this.GetPersonList();
+  }
+
+  async GetPersonList() {
+    const list = await GetAllPerson();
+    this.state.personList = list;
+    console.log(this.state);
   }
 
   async onSearch(text) {
-    console.log(await GetData(parseInt(text)));
+    // Wait until all found https://stackoverflow.com/questions/16149431/make-function-wait-until-element-exists
+    var checkExist = setInterval(async () => {
+      if (this.state.personList.length > 0) {
+        console.log("Exists!");
+        clearInterval(checkExist);
+         
+        // Get person
+        var foundList = this.state.personList.filter(e => e.name === text);
+        if (foundList.length > 0) {
+          console.log(await GetData(foundList[0].id));
+        } else {
+          console.log("Not Found: " + text);
+        }
+      }
+    }, 100);
   }
 
   render() {
--- suspect-expander/src/getData.js
+++ suspect-expander/src/getData.js
@@ -1,10 +1,46 @@
 import PlatformPath from 'path'
 
+const apiURL = "https://avatar.labpro.dev/"
+const friendsAPI = "friends/"
+const graphStartPoint = "1"
+
 // Based on: https://malcoded.com/posts/react-http-requests-axios/
-async function GetData (index){
-    const p = PlatformPath.join("https://avatar.labpro.dev/friends/", index.toString());
+// Get friends data from an index
+export async function GetData (index) {
+    const p = PlatformPath.join(apiURL, friendsAPI, index);
     const response = await fetch(p);
     return await response.json();
 }
 
-export default GetData;
+// Get all person from API
+export async function GetAllPerson () {
+    var list = await TraverseGraphLoop (graphStartPoint, []);
+    return list;
+}
+
+// Recursive function to get all person
+// Id is the id of the person
+// Currentlist is the currently tracked people data
+async function TraverseGraphLoop (id, currentList) {
+    if (currentList.some(e => e.id === id)) {
+        return currentList;
+    }
+
+    // Get Friends List
+    const response = await GetData(id);
+    var list = currentList;
+    if (response.status === 200)  {
+        // Save name in currentList
+        // console.log(list.filter(e => e.id === id)[0]);
+        currentList.push({id: id, name: response.payload.name});
+        var friends = response.payload.friends;
+        for (let i = 0; i < friends.length; i++) {
+            list = await TraverseGraphLoop(friends[i].id, list);
+            // console.log (friends[i].id);
+        }
+    } else if (response.status === 404) {
+        console.log("Missing: " + id);
+    }
+
+    return list;
+}
