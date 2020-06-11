631b670c6f9c6ff83a83788ebf82897192408c53
ozer0532 <ozer0532@planet.earth>
2020-06-11 13:15:04 +0700

    Implementasi menampilkan data teman

--- suspect-expander/src/App.js
+++ suspect-expander/src/App.js
@@ -13,19 +13,24 @@ const styles = {
     display: "flex",
     flexDirection: "column",
     background: 'white',
-    minHeight: '100vh',
+    height: '100vh',
   },
   searchContainer: {
     background: '#2196f3'
   },
   mainContainer: {
-    flex: 1,
+    flex: '0 1 100%',
+    overflowY: 'auto',
   },
   dataContainer: {
     background: '#fafafa',
+    overflowY: 'auto',
+    maxHeight: '100%',
   },
   graphContainer: {
     background: '#fff',
+  },
+  displayerContainer: {
   }
 };
 
@@ -34,6 +39,11 @@ class App extends React.Component {
     super(props);
     this.state = {
       personList: [],
+      currentPerson: {
+        id: "0",
+        name: "-",
+        friends: [],
+      },
     };
     this.GetPersonList = this.GetPersonList.bind(this);
     this.onSearch = this.onSearch.bind(this);
@@ -57,7 +67,11 @@ class App extends React.Component {
         // Get person
         let foundList = this.state.personList.filter(e => e.name === text);
         if (foundList.length > 0) {
-          console.log(await GetData(foundList[0].id));
+          let collectedData = await GetData(foundList[0].id);
+          console.log(collectedData);
+          this.setState({
+            currentPerson: collectedData.payload,
+          });
         } else {
           console.log("Not Found: " + text);
         }
@@ -75,7 +89,7 @@ class App extends React.Component {
         </Box>
         <Grid container spacing={0} style = { styles.mainContainer }>
           <Grid item xs={12} md={4} lg={3} style = {styles.dataContainer}>
-            <DataDisplayer />
+            <DataDisplayer data = { this.state.currentPerson } />
           </Grid>
           <Grid item xs={12} md={8} lg={9} style = { styles.graphContainer }>
             
--- suspect-expander/src/dataDisplayer.js
+++ suspect-expander/src/dataDisplayer.js
@@ -1,24 +1,113 @@
 import React from 'react';
 
 import Box from '@material-ui/core/Box'
+import Card from '@material-ui/core/Card'
+import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
 
 const styles = {
   mainContainer: {
     margin: '15px',
   },
+  spaced: {
+    marginTop: '15px',
+  },
+  card: {
+    padding: '10px',
+    marginTop: '10px',
+    marginBottom: '10px',
+  },
 }
 
 class DataDisplayer extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.RenderElements = this.RenderElements.bind(this);
+    this.RenderFriend = this.RenderFriend.bind(this);
+    this.RenderFriends = this.RenderFriends.bind(this);
+  }
+
+  RenderElements (props) {
+    if (props.data == null || props.data.name === "-") {
+      return "";
+    } else if ("element" in props.data) {
+      return "Elemen: " + props.data.element;
+    } else {
+      return "Elemen: N/A";
+    }
+  }
+
+  RenderFriend (props) {
+    return (
+      <Card style = { styles.card }>
+        <Typography 
+          variant = "subtitle2"
+          color="textSecondary"
+        >
+          { (props.index + 1) + '.' }
+        </Typography>
+        <Typography variant = 'h6'>
+          { props.entry.name }
+        </Typography>
+        <this.RenderElements data = { props.entry } />
+      </Card>
+    );
+  }
+
+  RenderFriends () {
+    if (this.props.data.name === "-") {
+      return "";
+    } else if ("friends" in this.props.data && this.props.data.friends.length > 0) {
+      return (
+        <Box style = { styles.spaced } >
+          <Typography variant='h6'> 
+            Rekan: 
+          </Typography>
+          {
+            this.props.data.friends.map((entry, index) => {
+              return (
+                <Box key = { index }>
+                  <this.RenderFriend 
+                    entry = { entry } 
+                    index = { index } 
+                  />
+                </Box>
+              );
+            })
+          }
+        </Box>
+      );
+    } else {
+      return (
+        <Box>
+          <Typography> Rekan: Tidak ada </Typography>
+        </Box>
+      );
+    }
+  }
+
   render () {
     return (
       <Box style = { styles.mainContainer }>
-        <Typography variant='subtitle1'>
+        {/* Top Row */}
+        <Typography 
+          variant='subtitle1' 
+          color="textSecondary" 
+        >
           Hasil Pencarian:
         </Typography>
-        <Typography variant='h4'>
-          Nama Orang Disini
+        <Typography variant='h5'>
+          { this.props.data.name }
         </Typography>
+        <Typography style = { styles.spaced }>
+          <this.RenderElements data = { this.props.data } />
+        </Typography>
+
+        <Divider style = { styles.spaced } />
+        {/* Bottom Row (Friends) */}
+        { this.RenderFriends() }
       </Box>
     );
   }
